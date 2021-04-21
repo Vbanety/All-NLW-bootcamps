db81d1f431523b6de3375411a404657235b4b915
Vinicius Batista <vbanetyy@gmail.com>
2021-04-21 10:48:52 -0300

    Finished the secund class about routes, migrations, and database

--- NLW#5/projeto_nlw/src/server.ts
+++ NLW#5/projeto_nlw/src/server.ts
@@ -1,20 +1,15 @@
-import express, { request, response } from 'express';
+import express from 'express';
+
+import './database';
+
+import { routes } from "./routes";
 
 const app = express();
 
-// creating routes GET = Search / POST = CREATE / PUT = Alter / DELETE = Drop / PATCH = Alter a specific information
+app.use(express.json());
 
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Hey dude, how's it going? Let's try to build something with node.js?"
-    })
-});
+app.use(routes);
 
-app.post("/users", (request, response) => {
-    return response.json({
-        message: "user created successfull!!"
-    });
-})
 
 //SERVER 
 app.listen(3333, () => console.log("Server is running on port 3333"));
